23a72a47d663284d14d713db5f6ca0b3b85383ee
Amalshabanah <amalshabanah7@gmail.com>
2022-08-27 11:16:34 +0300

    StepOne - -Make Three tests for "", "num", "num, num" cases. - implement the StringCalcualtor.cs with the needed code.

---
+++ StringCalcualtor.Test/Serivces/StringCalcualtor.cs
@@ -0,0 +1,23 @@
+namespace StringCalcualtor.Serivces;
+public class StringCalcualtor
+{
+    public int Add(string numbers)
+    {
+        if (string.IsNullOrEmpty(numbers))
+            return 0;
+        if (!numbers.Contains(","))
+            return Int32.Parse(numbers);
+        else
+        {
+            int sum = 0;
+            string[] nums;
+            nums = numbers.Split(',');
+            foreach (var num in nums)
+            {
+                sum += Int32.Parse(num);
+            }
+
+            return sum;
+        }
+    }
+}
---
+++ StringCalcualtor.Test/Tests/StringCalcualtorTests.cs
@@ -0,0 +1,32 @@
+global using Xunit;
+
+namespace StringCalcualtor.Serivces;
+public class StringCalcualtorTests
+{
+    private readonly StringCalcualtor _stringCalculator;
+    public StringCalcualtorTests()
+    {
+        _stringCalculator = new StringCalcualtor();
+    }
+    
+    [Fact]
+    [InlineData(0, "")]
+    public void ShouldReturnZero()
+    {
+        var result = _stringCalculator.Add("");
+        Assert.Equal(0, result);
+    }
+    
+    [InlineData(1,"1")]
+    public void ShouldReturnOneNumber()
+    {
+        var result = _stringCalculator.Add("1");
+        Assert.Equal(1, result);
+    }
+
+    public void ShouldReturnSum()
+    {
+        var result = _stringCalculator.Add("1, 2");
+        Assert.Equal(3, result);
+    }
+}
