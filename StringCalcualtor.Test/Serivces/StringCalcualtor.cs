017e9fa9c521ce83ae397e3c52136cde0b788c57
Amalshabanah <amalshabanah7@gmail.com>
2022-08-28 16:20:47 +0300

    StepOne - Change the order of the expected data and the parameter - move variables to the beginign of the method.

--- StringCalcualtor.Test/Serivces/StringCalcualtor.cs
+++ StringCalcualtor.Test/Serivces/StringCalcualtor.cs
@@ -2,15 +2,16 @@ namespace StringCalcualtor.Serivces;
 public class StringCalcualtor
 {
     public int Add(string numbers)
-    {
+    { 
+        var sum = 0;
+        var nums = numbers.Split(',');
+        
         if (string.IsNullOrEmpty(numbers))
             return 0;
         
         if (!numbers.Contains(","))
             return Int32.Parse(numbers);
-        
-        var sum = 0;
-        var nums = numbers.Split(',');
+       
         foreach (var num in nums)
         {
             sum += Int32.Parse(num);
--- StringCalcualtor.Test/Tests/StringCalcualtorTests.cs
+++ StringCalcualtor.Test/Tests/StringCalcualtorTests.cs
@@ -11,25 +11,25 @@ public class StringCalcualtorTests
     }
     
     [Theory]
-    [InlineData(0, "")]
-    public void ShouldReturnZero(int expected, string number)
+    [InlineData("", 0)]
+    public void ShouldReturnZero(string number, int expected)
     {
         var result = _stringCalculator.Add(number);
         Assert.Equal(expected, result);
     }
     
     [Theory]
-    [InlineData(1,"1")]
-    public void ShouldReturnOneNumber(int expected, string number)
+    [InlineData("1", 1)]
+    public void ShouldReturnOneNumber(string number, int expected)
     {
         var result = _stringCalculator.Add(number);
         Assert.Equal(expected, result);
     }
     
     [Theory]
-    [InlineData(3,"1, 2")]
-    [InlineData(5,"1,4")]
-    public void ShouldReturnSum(int expected, string number)
+    [InlineData("1, 2", 3)]
+    [InlineData("1,4", 5)]
+    public void ShouldReturnSum(string number, int expected)
     {
         var result = _stringCalculator.Add(number);
         Assert.Equal(expected, result);
