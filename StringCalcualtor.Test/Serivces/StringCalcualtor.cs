2bbf5b4472ab703e579b4bf7b2fec6459921d582
Amalshabanah <amalshabanah7@gmail.com>
2022-08-29 12:31:04 +0300

    StepTwo - syncing with master.

--- StringCalcualtor.Test/Serivces/StringCalcualtor.cs
+++ StringCalcualtor.Test/Serivces/StringCalcualtor.cs
@@ -4,12 +4,12 @@ public class StringCalcualtor : IStringCalculator
     public int Add(string numbers)
     { 
         var sum = 0;
-        var nums = numbers.Split(',');
+        var nums = numbers.Split(new char[] {',', '\n'});
         
         if (string.IsNullOrEmpty(numbers))
             return 0;
         
-        if (!numbers.Contains(","))
+        if (!numbers.Contains(",") && !numbers.Contains("\n"))
             return Int32.Parse(numbers);
        
         foreach (var num in nums)
